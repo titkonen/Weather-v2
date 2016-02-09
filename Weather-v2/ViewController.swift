db48bc350c955b70a3300d9d17b982ca54b0f71d
titkonen <toni.itkonen@icloud.com>
2016-02-09 20:02:49 +0200

    Initial Commit

---
+++ Weather-v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather-v2
+//
+//  Created by Toni Itkonen on 09/02/16.
+//  Copyright © 2016 Kirijaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
